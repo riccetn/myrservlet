0a3fb13139a8ed46113143cc8bab94e0f699162f
Rickard Närström <rickard@narstrom.se>
2025-05-19 21:01:17 +0200

    synchronized -> Lock

--- src/main/java/se/narstrom/myr/servlet/AsyncHandler.java
+++ src/main/java/se/narstrom/myr/servlet/AsyncHandler.java
@@ -1,6 +1,9 @@
 package se.narstrom.myr.servlet;
 
 import java.io.IOException;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import jakarta.servlet.AsyncContext;
 import jakarta.servlet.AsyncListener;
@@ -12,6 +15,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public final class AsyncHandler implements AsyncContext {
+	private final Lock lock = new ReentrantLock();
+
+	private final Condition cond = lock.newCondition();
+
 	private Request originalRequest = null;
 
 	private Response originalResponse = null;
@@ -26,74 +33,115 @@ public final class AsyncHandler implements AsyncContext {
 
 	private String path = null;
 
-	synchronized void service(final Request request, final Response response) throws ServletException, IOException, InterruptedException {
-		if (state != State.DISPATCHING)
-			throw new IllegalStateException();
-		this.request = this.originalRequest = request;
-		this.response = this.originalResponse = response;
-		this.context = request.getServletContext();
+	void service(final Request request, final Response response) throws ServletException, IOException, InterruptedException {
+		lock.lock();
+		try {
+			if (state != State.DISPATCHING)
+				throw new IllegalStateException();
+			this.request = this.originalRequest = request;
+			this.response = this.originalResponse = response;
+			this.context = request.getServletContext();
 
-		final String uri = request.getRequestURI();
-		final String contextPath = context.getContextPath();
+			final String uri = request.getRequestURI();
+			final String contextPath = context.getContextPath();
 
-		assert uri.startsWith(contextPath);
+			assert uri.startsWith(contextPath);
 
-		this.path = uri.substring(contextPath.length());
+			this.path = uri.substring(contextPath.length());
 
-		while (state == State.DISPATCHING) {
-			final Dispatcher dispatcher = context.getRequestDispatcher(path);
-			originalRequest.setAsyncSupported(dispatcher.getRegistration().isAsyncSupported());
+			while (state == State.DISPATCHING) {
 
-			this.state = State.DISPATCHED;
+				final Dispatcher dispatcher = context.getRequestDispatcher(path);
+				originalRequest.setAsyncSupported(dispatcher.getRegistration().isAsyncSupported());
 
-			context.getRequestDispatcher(path).request(this.request, this.response);
+				this.state = State.DISPATCHED;
 
-			state = switch (state) {
-				case DISPATCHED, COMPLETING -> State.COMPLETED;
-				case ASYNC_STARTED -> State.ASYNC_WAIT;
-				case REDISPATCHING -> State.DISPATCHING;
-				default -> throw new IllegalStateException();
-			};
+				lock.unlock();
+				try {
+					dispatcher.request(this.request, this.response);
+				} finally {
+					lock.lock();
+				}
 
-			while (state == State.ASYNC_WAIT)
-				wait();
+				state = switch (state) {
+					case DISPATCHED, COMPLETING -> State.COMPLETED;
+					case ASYNC_STARTED -> State.ASYNC_WAIT;
+					case REDISPATCHING -> State.DISPATCHING;
+					default -> throw new IllegalStateException();
+				};
+
+				while (state == State.ASYNC_WAIT)
+					cond.await();
+			}
+
+			assert state == State.COMPLETED;
+		} finally {
+			lock.unlock();
 		}
-
-		assert state == State.COMPLETED;
 	}
 
-	synchronized void startAsync() {
-		if (state != State.DISPATCHED)
-			throw new IllegalStateException();
-		this.state = State.ASYNC_STARTED;
+	void startAsync() {
+		lock.lock();
+		try {
+			if (state != State.DISPATCHED)
+				throw new IllegalStateException();
+			this.state = State.ASYNC_STARTED;
+		} finally {
+			lock.unlock();
+		}
 	}
 
-	synchronized void startAsync(final ServletRequest request, final ServletResponse response) {
-		startAsync();
-		this.request = (HttpServletRequest) request;
-		this.response = (HttpServletResponse) response;
+	void startAsync(final ServletRequest request, final ServletResponse response) {
+		lock.lock();
+		try {
+			startAsync();
+			this.request = (HttpServletRequest) request;
+			this.response = (HttpServletResponse) response;
+		} finally {
+			lock.unlock();
+		}
 	}
 
-	synchronized boolean isAsyncStarted() {
-		return switch (state) {
-			case ASYNC_STARTED, REDISPATCHING, COMPLETING -> true;
-			default -> false;
-		};
+	boolean isAsyncStarted() {
+		lock.lock();
+		try {
+			return switch (state) {
+				case ASYNC_STARTED, REDISPATCHING, COMPLETING -> true;
+				default -> false;
+			};
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
-	public synchronized ServletRequest getRequest() {
-		return request;
+	public ServletRequest getRequest() {
+		lock.lock();
+		try {
+			return request;
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
-	public synchronized ServletResponse getResponse() {
-		return response;
+	public ServletResponse getResponse() {
+		lock.lock();
+		try {
+			return response;
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
-	public synchronized boolean hasOriginalRequestAndResponse() {
-		return request == originalRequest && response == originalResponse;
+	public boolean hasOriginalRequestAndResponse() {
+		lock.lock();
+		try {
+			return request == originalRequest && response == originalResponse;
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
@@ -115,25 +163,35 @@ public final class AsyncHandler implements AsyncContext {
 	}
 
 	@Override
-	public synchronized void dispatch(final ServletContext context, final String path) {
-		state = switch (state) {
-			case ASYNC_STARTED -> State.REDISPATCHING;
-			case ASYNC_WAIT -> State.DISPATCHING;
-			default -> throw new IllegalStateException();
-		};
-		this.context = (Context) context;
-		this.path = path;
-		this.notifyAll();
+	public void dispatch(final ServletContext context, final String path) {
+		lock.lock();
+		try {
+			state = switch (state) {
+				case ASYNC_STARTED -> State.REDISPATCHING;
+				case ASYNC_WAIT -> State.DISPATCHING;
+				default -> throw new IllegalStateException();
+			};
+			this.context = (Context) context;
+			this.path = path;
+			cond.signalAll();
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
-	public synchronized void complete() {
-		state = switch (state) {
-			case ASYNC_STARTED -> State.COMPLETING;
-			case ASYNC_WAIT -> State.COMPLETED;
-			default -> throw new IllegalStateException();
-		};
-		this.notifyAll();
+	public void complete() {
+		lock.lock();
+		try {
+			state = switch (state) {
+				case ASYNC_STARTED -> State.COMPLETING;
+				case ASYNC_WAIT -> State.COMPLETED;
+				default -> throw new IllegalStateException();
+			};
+			cond.signalAll();
+		} finally {
+			lock.unlock();
+		}
 	}
 
 	@Override
